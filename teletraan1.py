77e7723f30a62f3226e62152294c3d1e24df1c4f
elliott cordo <elliottcordo@gmail.com>
2016-03-14 10:20:24 -0400

    added host='0.0.0.0', debug = False so it works in ec2

--- teletraan1.py
+++ teletraan1.py
@@ -163,4 +163,4 @@ def log(message):
     print(message)
 #------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug = False)
